3ea8cea459e765a3c11bbfba46367e4cf3b8a673
Tran Tung <trantung1197@gmail.com>
2020-10-24 17:14:34 +0700

    search bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,24 +16,39 @@ const btnCloseToggle = getEle(".close__toggle");
 
 btnToggle.addEventListener("click", () => {
 	bodyContainer.classList.add("open-menu");
-    navMenu.classList.toggle("is-closed");
+	navMenu.classList.toggle("is-closed");
 });
 
 btnCloseToggle.addEventListener("click", () => {
 	bodyContainer.classList.remove("open-menu");
-    navMenu.classList.add("is-closed");
+	navMenu.classList.add("is-closed");
 });
 
 document.body.addEventListener("click", (event) => {
-    var closestNavMenu = event.target.closest(".nav__menu");
-    var closestBtnToggle = event.target.closest(".nav__toggler");
+	var closestNavMenu = event.target.closest(".nav__menu");
+	var closestBtnToggle = event.target.closest(".nav__toggler");
 
-    if (closestNavMenu !== null || closestBtnToggle !== null) {
-        return;
-    } else {
-        bodyContainer.classList.remove("open-menu");
-        navMenu.classList.add("is-closed");
-    }
+	if (closestNavMenu !== null || closestBtnToggle !== null) {
+		return;
+	} else {
+		bodyContainer.classList.remove("open-menu");
+		navMenu.classList.add("is-closed");
+	}
 });
 
 /* Search button */
+const btnCloseSearchBar = getEle(".close-search-bar");
+const labelSearch = getEle(".label-search");
+const searchFormContainer = getEle(".search-form");
+const btnOpenSearch = getEle('.btn-search');
+
+btnCloseSearchBar.addEventListener("click", () => {
+	console.log(labelSearch);
+	searchFormContainer.classList.add("is-closed");
+	bodyContainer.classList.remove("open-search");
+});
+
+btnOpenSearch.addEventListener('click',()=>{
+    searchFormContainer.classList.remove('is-closed');
+    bodyContainer.classList.add('open-search');
+});
