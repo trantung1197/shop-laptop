c9d44e324378609e4aa0136b327fb119060ae7d9
Tran Tung <trantung1197@gmail.com>
2020-10-25 17:25:08 +0700

    opacity

--- assets/js/slide-carousel.js
+++ assets/js/slide-carousel.js
@@ -6,11 +6,11 @@ let carouselSlide = getEle(".carousel__slide");
 let listSlideItem = getListEle(".slide__item");
 let carouselLenght = listSlideItem.length;
 let sizeItem = window.innerWidth;
-let counter = 1;
-let isLoop = true;
+let isLoop = false;
 let timeTrans = 1;
 let timeInterval = 5000;
-let autoSlide = true;
+let autoSlide = false;
+let counter = 0;
 
 /* function get element */
 getEle = (params) => {
@@ -27,23 +27,33 @@ removeClassAtListItem = (listNode, className) => {
 	});
 };
 
-//create navigation button radio
+/* Reset width item */
+resetWidthOfSlideItem = (list, width) => {
+	list.forEach((ele) => {
+		ele.style.width = `${width}px`;
+	});
+};
+
+/* create navigation button radio */
 createInputRadio = (id) => {
 	return `
     <input type="radio" id=${id} name="radio-btn">
     <label for="${id}" class="manual-btn"></label>
     `;
 };
+/** Create list Radio button **/
+createListRadioBtn = (number) => {
+	let htmlStringRadioBtn = "";
+	for (let i = 1; i <= number; i++) {
+		htmlStringRadioBtn += createInputRadio(`radio-${i}`);
+	}
+	return htmlStringRadioBtn;
+};
 
-let htmlStringRadioBtn = "";
-for (let i = 1; i <= carouselLenght; i++) {
-	htmlStringRadioBtn += createInputRadio(`radio-${i}`);
-}
+navigationBtnBox.innerHTML = createListRadioBtn(carouselLenght);
+let labelBtnRadio = getListEle("input[name=radio-btn]");
 
-navigationBtnBox.innerHTML = htmlStringRadioBtn;
-let labelBtnRadio = document.querySelectorAll("input[name=radio-btn]");
-
-//set cheked input radio
+/* set cheked input radio */
 setCheckedRadio = (index) => {
 	labelBtnRadio.forEach((ele) => {
 		ele.removeAttribute("checked");
@@ -61,6 +71,10 @@ setCheckedRadio(0);
 
 /** clone item slide **/
 if (isLoop) {
+	//start at first element = index: 1
+	counter = 1;
+	carouselSlide.style.transform = `translateX(${-sizeItem * counter}px)`;
+
 	let firstSlideChildClone = carouselSlide.firstElementChild.cloneNode(true);
 	let lastSlideChildClone = carouselSlide.lastElementChild.cloneNode(true);
 	carouselSlide.append(firstSlideChildClone);
@@ -73,93 +87,94 @@ if (isLoop) {
 	listSlideItem[0].classList.add("clone-last");
 }
 
-listSlideItem.forEach((ele) => {
-	ele.style.width = `${sizeItem}px`;
-});
+resetWidthOfSlideItem(listSlideItem, sizeItem);
 
 window.onresize = function (event) {
 	sizeItem = window.innerWidth;
-	listSlideItem.forEach((ele) => {
-		ele.style.width = `${sizeItem}px`;
-    });
-    
-	//start at first element = index: 1
-	carouselSlide.style.transform = `translateX(${-sizeItem * counter}px)`;
+	resetWidthOfSlideItem(listSlideItem, sizeItem);
 };
 
-//start at first element = index: 1
-carouselSlide.style.transform = `translateX(${-sizeItem * counter}px)`;
+// //start at first element = index: 1
+// carouselSlide.style.transform = `translateX(${-sizeItem * counter}px)`;
 
-transformSlide = (isNext) => {
-	if (isNext) {
-		if (counter >= carouselLenght - 1) return;
-		counter++;
-	} else {
-		if (counter <= 0) return;
-		counter--;
-	}
-	// listSlideItem[counter-1].style.animation = `fadeOut ${timeTrans}s ease-in-out`;
-	
-	carouselSlide.style.transition = `all ${timeTrans}s ease-in-out`;
-	carouselSlide.style.transform = `translateX(${-sizeItem * counter}px)`;
-	removeClassAtListItem(listSlideItem, 'active');
-	listSlideItem[counter].classList.add('active');
-	setCheckedRadio(counter - 1);
-};
+// transformSlide = (isNext) => {
+// 	if (isNext) {
+// 		if (counter >= carouselLenght - 1) return;
+// 		counter++;
+// 	} else {
+// 		if (counter <= 0) return;
+// 		counter--;
+// 	}
+// 	// listSlideItem[counter-1].style.animation = `fadeOut ${timeTrans}s ease-in-out`;
 
-transformSlideRadio = (num) => {
-	carouselSlide.style.transition = `all ${timeTrans}s ease-in-out`;
-	carouselSlide.style.transform = `translateX(${-sizeItem * num}px)`;
-};
+// 	carouselSlide.style.transition = `none`;
+// 	carouselSlide.style.transform = `translateX(${-sizeItem * counter}px)`;
+// 	removeClassAtListItem(listSlideItem, "active");
+// 	listSlideItem[counter].classList.add("active");
+
+// 	console.log(counter);
+
+// 	setCheckedRadio(counter);
+// };
+
+// transformSlideRadio = (num) => {
+// 	carouselSlide.style.transition = `none`;
+// 	// carouselSlide.style.transition = `all ${timeTrans}s ease-in-out`;
+// 	carouselSlide.style.transform = `translateX(${-sizeItem * num}px)`;
+// };
 
 //onWheel
-carouselSlide.addEventListener("wheel", (event) => {
-	if (event.deltaY < 0) {
-		transformSlide(true);
-	} else {
-		transformSlide(false);
-	}
-});
+// carouselSlide.addEventListener("wheel", (event) => {
+// 	if (event.deltaY < 0) {
+// 		transformSlide(true);
+// 	} else {
+// 		transformSlide(false);
+// 	}
+// });
 
-/** Button next - prev **/
+// /** Button next - prev **/
 btnNext.addEventListener("click", () => {
-	transformSlide(true);
+	// transformSlide(true);
+	console.log(counter);
+	counter++;
+	removeClassAtListItem(listSlideItem, "active");
+	listSlideItem[counter].classList.add("active");
 });
 
-btnPrev.addEventListener("click", () => {
-	transformSlide(false);
-});
+// btnPrev.addEventListener("click", () => {
+// 	transformSlide(false);
+// });
 
-//reset loop
-carouselSlide.addEventListener("transitionend", () => {
-	if (isLoop) {
-		if (listSlideItem[counter].classList.contains("clone-last")) {
-			carouselSlide.style.transition = "none";
-			counter = carouselLenght - 2;
-			carouselSlide.style.transform = `translateX(${
-				-sizeItem * counter
-			}px)`;
-		} else if (listSlideItem[counter].classList.contains("clone-first")) {
-			carouselSlide.style.transition = "none";
-			counter = 1;
-			carouselSlide.style.transform = `translateX(${
-				-sizeItem * counter
-			}px)`;
-		}
-	}
-});
+// //reset loop
+// carouselSlide.addEventListener("transitionend", () => {
+// 	if (isLoop) {
+// 		if (listSlideItem[counter].classList.contains("clone-last")) {
+// 			carouselSlide.style.transition = "none";
+// 			counter = carouselLenght - 2;
+// 			carouselSlide.style.transform = `translateX(${
+// 				-sizeItem * counter
+// 			}px)`;
+// 		} else if (listSlideItem[counter].classList.contains("clone-first")) {
+// 			carouselSlide.style.transition = "none";
+// 			counter = 1;
+// 			carouselSlide.style.transform = `translateX(${
+// 				-sizeItem * counter
+// 			}px)`;
+// 		}
+// 	}
+// });
 
-// auto loop
-if (autoSlide & isLoop) {
-	setInterval(() => {
-		transformSlide(true);
-	}, timeInterval);
-}
+// // auto loop
+// if (autoSlide & isLoop) {
+// 	setInterval(() => {
+// 		transformSlide(true);
+// 	}, timeInterval);
+// }
 
-//navigation button
-labelBtnRadio.forEach((element) => {
-	element.addEventListener("click", (event) => {
-		counter = event.target.id.match(/\d+/)[0];
-		transformSlideRadio(counter);
-	});
-});
+// //navigation button
+// labelBtnRadio.forEach((element) => {
+// 	element.addEventListener("click", (event) => {
+// 		counter = event.target.id.match(/\d+/)[0];
+// 		transformSlideRadio(counter);
+// 	});
+// });
